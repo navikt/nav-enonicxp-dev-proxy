1fd5d7cf9a2937221e1fd6c5710ea821d0c30002
Anders Bråthen Nomerstad <anders@nomerstad.com>
2023-05-09 16:17:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nav-enonicxp dev proxy
+
+Proxy til nav-enonicxp dev-miljøer, for bruk ved lokal frontend-utvikling
---
+++ src/server.ts
@@ -0,0 +1,45 @@
+import express, { ErrorRequestHandler } from 'express';
+
+const app = express();
+const port = 1337;
+
+app.get('/internal/isAlive', (req, res) => {
+    return res.status(200).send('I am alive!');
+});
+
+app.get('/internal/isReady', (req, res) => {
+    return res.status(200).send('I am ready!');
+});
+
+app.get('*', (req, res) => {
+    console.log(`Hello! ${req.url}`);
+    return res.status(200).send();
+});
+
+app.use(((err, req, res, _) => {
+    const {path} = req;
+    const {status, stack} = err;
+    const msg = stack?.split('\n')[0];
+
+    console.log(`Express error on path ${path}: ${status} ${msg}`);
+
+    const statusCode = status || 500;
+
+    return res.status(statusCode).send(msg);
+}) as ErrorRequestHandler);
+
+const server = app.listen(port, () => {
+    console.log(`Server starting on port ${port}`);
+});
+
+const shutdown = () => {
+    console.log('Server shutting down');
+
+    server.close(() => {
+        console.log('Shutdown complete!');
+        process.exit(0);
+    });
+};
+
+process.on('SIGTERM', shutdown);
+process.on('SIGINT', shutdown);
